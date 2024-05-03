8c73d2c80a8a780310794ca0bf1f75a7671fae30
Larry Versaw <lversaw@gmail.com>
2024-05-03 10:35:38 -0600

    framework for Word to txt conversion

---
+++ src/g_word2text.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+# Implements Word2text and Word2text_Frame, which are the controller and frame
+# for operating the word2text.py script.
+# GUI interface for converting Word documents to text.
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import font
+from tkinter import filedialog
+from idlelib.tooltip import Hovertip
+import os
+import g_util
+import g_step
+
+stepname = 'Word2text'   # equals the main class name in this module
+
+class Word2text(g_step.Step):
+    def __init__(self, mainframe, mainapp):
+        super().__init__(mainframe, mainapp, stepname, "Convert Word docs to text")
+        self.frame = Word2text_Frame(parent=mainframe, controller=self)
+        self.frame.grid(row=1, column=0, sticky="nsew")
+
+    def name(self):
+        return stepname
+
+    def onExecute(self, values):
+        self.enablebutton(2, False)
+        count = 1
+        if not values['filename']:
+            count = g_util.count_files(values['source_dir'], ".*docx$")
+        self.mainapp.execute_script("word2text", count)
+        self.frame.clear_messages()
+    def onNext(self):
+        copyparms = {'source_dir': self.values['target_dir']}
+        self.mainapp.step_next(copyparms)
+
+    # Called by the main app.
+    def onScriptEnd(self, status: str):
+        if not status:
+            status = f"The conversion is done.\nNow prepare the text files for the next step."
+        self.frame.show_progress(status)
+        self.frame.onScriptEnd()
+        self.enablebutton(2, True)
+                
+class Word2text_Frame(g_step.Step_Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+
+        self.filename = StringVar()
+        self.source_dir = StringVar()
+        self.target_dir = StringVar()
+        for var in (self.source_dir, self.target_dir):
+            var.trace_add("write", self._onChangeEntry)
+        for col in [3,4]:
+            self.columnconfigure(col, weight=1)   # keep column 1 from expanding
+
+        source_dir_label = ttk.Label(self, text="Location of .docx files:", width=20)
+        source_dir_label.grid(row=3, column=1, sticky=W, pady=2)
+        source_dir_entry = ttk.Entry(self, width=42, textvariable=self.source_dir)
+        source_dir_entry.grid(row=3, column=2, columnspan=3, sticky=W)
+        target_dir_Tip = Hovertip(source_dir_entry, hover_delay=1000,
+                text="Folder containing the files to be converted")
+        src_dir_find = ttk.Button(self, text="...", width=2, command=self._onFindSrcDir)
+        src_dir_find.grid(row=3, column=4, sticky=W, padx=5)
+
+        file_label = ttk.Label(self, text="File name:", width=20)
+        file_label.grid(row=4, column=1, sticky=W, pady=2)
+        file_entry = ttk.Entry(self, width=20, textvariable=self.filename)
+        file_entry.grid(row=4, column=2, sticky=W)
+        file_Tip = Hovertip(file_entry, hover_delay=500,
+             text="Leave filename blank to convert all .txt files in the folder.")
+        file_find = ttk.Button(self, text="...", width=2, command=self._onFindFile)
+        file_find.grid(row=4, column=3, sticky=W, padx=8)
+
+        target_dir_label = ttk.Label(self, text="Location for text files:", width=21)
+        target_dir_label.grid(row=5, column=1, sticky=W, pady=2)
+        target_dir_entry = ttk.Entry(self, width=42, textvariable=self.target_dir)
+        target_dir_entry.grid(row=5, column=2, columnspan=3, sticky=W)
+        target_dir_Tip = Hovertip(target_dir_entry, hover_delay=1000,
+                text="Folder for the new text files. The folder will be created if it doesn't exist.")
+        target_dir_find = ttk.Button(self, text="...", width=2, command=self._onFindTargetDir)
+        target_dir_find.grid(row=5, column=4, sticky=W, padx=5)
+
+        source_dir_entry.focus()
+
+    # Called when the frame is first activated. Populate the initial values.
+    def show_values(self, values):
+        self.values = values
+        self.filename.set(values['filename'])
+        self.source_dir.set(values['source_dir'])
+        self.target_dir.set(values['target_dir'])
+        
+        # Create buttons
+        self.controller.showbutton(1, "<<<", cmd=self._onBack)
+        self.controller.showbutton(2, "CONVERT", tip="Run the conversion script now.", cmd=self._onExecute)
+        self.controller.showbutton(3, "Source folder",
+                                   tip="Open the folder of Word docs.", cmd=self._onOpenTextDir)
+        self.controller.showbutton(4, "Target folder", cmd=self._onOpenTargetDir)
+        self.controller.showbutton(5, ">>>", tip="Convert the text files to usfm.", cmd=self._onSkip)
+        self._set_button_status()
+
+    # Caches the current parameters in self.values and calls the mainapp to save them in the config file.
+    def _save_values(self):
+        self.values['filename'] = self.filename.get()
+        self.values['source_dir'] = self.source_dir.get()
+        self.values['target_dir'] = self.target_dir.get()
+        self.controller.mainapp.save_values(stepname, self.values)
+        self._set_button_status()
+
+    def _onFindSrcDir(self, *args):
+        self.controller.askdir(self.source_dir)
+    def _onFindTargetDir(self, *args):
+        self.controller.askdir(self.target_dir)
+    def _onFindFile(self, *args):
+        path = filedialog.askopenfilename(initialdir=self.source_dir.get(), title = "Select document",
+                                           filetypes=[('Word file', '*.docx')])
+        if path:
+            self.filename.set(os.path.basename(path))
+    def _onChangeEntry(self, *args):
+        self._set_button_status()
+    def _onOpenTextDir(self, *args):
+        os.startfile(self.source_dir.get())
+    def _onOpenTargetDir(self, *args):
+        self._save_values()
+        os.startfile(self.values['target_dir'])
+    def onScriptEnd(self):
+        self.message_area['state'] = DISABLED   # prevents insertions to message area
+        self.controller.showbutton(5, ">>>", tip="Convert the text files to usfm.", cmd=self._onNext)
+
+    def _set_button_status(self):
+        good_sourcedir = os.path.isdir(self.source_dir.get())
+        okay = (good_sourcedir and self.target_dir.get())
+        self.controller.enablebutton(2, okay)
+        self.controller.enablebutton(3, good_sourcedir)
+        self.controller.enablebutton(4, os.path.isdir(self.target_dir.get()))
---
+++ src/word2text.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+# This script converts Word documents to UTF-8 text files.
+
+import configmanager
+import usfm_verses
+import re
+import io
+import os
+import sys
+from pathlib import Path
+from docx import Document
+
+config = None
+gui = None
+ids = {}    # id: list of fnames for that id
+issues_file = None
+
+# Writes error message to stderr and to issues.txt.
+def reportError(msg):
+    reportToGui('<<ScriptMessage>>', msg)
+    write(msg, sys.stderr)
+    openIssuesFile().write(msg + "\n")
+
+# Sends a progress message to the GUI, and to stdout.
+def reportProgress(msg):
+    reportToGui('<<ScriptProgress>>', msg)
+    write(msg, sys.stdout)
+
+# Sends a status message to the GUI, and to stdout.
+def reportStatus(msg):
+    reportToGui('<<ScriptMessage>>', msg)
+    write(msg, sys.stdout)
+
+def reportToGui(event, msg):
+    if gui:
+        with gui.progress_lock:
+            gui.progress = msg if not gui.progress else f"{gui.progress}\n{msg}"
+        gui.event_generate(event, when="tail")
+
+# This little function streams the specified message and handles UnicodeEncodeError
+# exceptions, which are common in Indian language texts. 2/5/24.
+def write(msg, stream):
+    try:
+        stream.write(msg + "\n")
+    except UnicodeEncodeError as e:
+        stream.write("(Unicode...)\n")
+
+# If issues.txt file is not already open, opens it for writing.
+# First renames existing issues.txt file to issues-oldest.txt unless
+# issues-oldest.txt already exists.
+# Returns file pointer.
+def openIssuesFile():
+    global issues_file
+    if not issues_file:
+        path = os.path.join(config['source_dir'], "issues.txt")
+        if os.path.exists(path):
+            bakpath = os.path.join(config['source_dir'], "issues-oldest.txt")
+            if not os.path.exists(bakpath):
+                os.rename(path, bakpath)
+        issues_file = io.open(path, "tw", buffering=2048, encoding='utf-8', newline='\n')
+    return issues_file
+
+# Reports error if this bookId has already been seen.
+def check_dups(bookId, fname):
+    global ids
+    if bookId in ids:
+        ids[bookId].append(fname)
+        num =  usfm_verses.verseCounts[bookId]['usfm_number']
+        msg = f"Multiple file names resolved to {num}-{bookId}:"
+        for name in ids[bookId]:
+            msg += f"\n  {name}"
+        reportError(msg)
+    else:
+        ids[bookId] = [fname]
+
+# Returns the degree of similarity between the two strings by a simple algorithm,
+# similar to Jaccard similarity metric, based on number of characters in common.
+def similarity(s1, s2):
+    set1 = set(list(s1.upper()))
+    set2 = set(list(s2.upper()))
+    sim = len(set1&set2) / len(set1|set2)
+    return sim
+
+# Returns the most likely bookId for the specified number and fname.
+def bookByNumber(num, name, bookIds):
+    books = usfm_verses.verseCounts
+
+    for id in bookIds:
+        if num == books[id]["usfm_number"]:
+            bookId = id
+            break
+    if int(num) > 39:   # NT
+        b2 = None
+        for book in books:
+            if books[book]['sort'] == int(num):
+                b2 = book
+                break
+        sim1 = similarity(name, books[bookId]['en_name'])
+        if similarity(name, books[b2]['en_name']) > sim1:
+            bookId = b2
+    return bookId
+
+num_name_re = re.compile(r'([0-6][0-9]?)[ \-\.]*([A-Za-z1-3][ A-Za-z][A-Za-z])')
+
+# Attempts to determine the Bible book from the file name.
+# Returns upper case bookId, or empty string on failure.
+def getBookId(filename):
+    bookId = None
+    (fname, ext) = os.path.splitext(filename)
+    books = usfm_verses.verseCounts
+    bookIds = list(books)
+    bookIds.sort(key=lambda book: len(books[book]['en_name']), reverse=True)
+    if len(fname) == 3 and fname.upper() in bookIds:
+        bookId = fname.upper()
+    else:
+        for id in bookIds:
+            if books[id]['en_name'] in fname.title():
+                bookId = id
+                break
+    if not bookId:
+        if num_name := num_name_re.match(fname):
+            num2 = int(num_name.group(1))
+            name3 = num_name.group(2).upper()
+            if name3 in books:
+                bookId = name3
+            elif 0 < num2 < 68:
+                bookId = bookByNumber(f"{num2:02d}", fname[fname.find(num_name.group(2)):], bookIds)
+    return bookId
+
+def shortname(longpath):
+    source_dir = Path(config['source_dir'])
+    shortname = Path(longpath)
+    if shortname.is_relative_to(source_dir):
+        shortname = shortname.relative_to(source_dir)
+    return str(shortname)
+
+# Generates a path name for output text file.
+def makeTxtPath(inputpath, bookId):
+    if bookId:
+        num = usfm_verses.verseCounts[bookId]['usfm_number']
+        path = os.path.join(config['target_dir'], f"{num}-{bookId}.txt")
+    else:
+        fname = os.path.basename(inputpath) + ".txt"
+        path = os.path.join(config['target_dir'], fname)
+    return path
+
+# This method is called to convert the specified file to usfm.
+def convertFile(path, bookId):
+    reportProgress(f"Converting: {shortname(path)}...")
+    sys.stdout.flush()
+    if not bookId:
+        reportError("Unable to identify this book from the file name.")
+    txtpath = makeTxtPath(path, bookId)
+    with io.open(txtpath, 'tw', encoding='utf-8', newline='\n') as output:
+        output.write(f"The text in this file was extracted from {path}.\n")
+
+def convertFolder(folder):
+    for entry in os.listdir(folder):
+        path = os.path.join(folder, entry)
+        if os.path.isdir(path) and entry[0] != '.':
+            convertFolder(path)
+        elif os.path.isfile(path) and entry.endswith(".docx"):
+            bookId = getBookId(entry)
+            if bookId:
+                check_dups(bookId, entry)
+            convertFile(path, bookId)
+
+def main(app = None):
+    global gui
+    gui = app
+    global ids
+    ids = {}
+    global issues_file
+    issues_file = None
+    global config
+    config = configmanager.ToolsConfigManager().get_section('Word2text')
+    if config:
+        source_dir = config['source_dir']
+        file = config['filename']
+        target_dir = config['target_dir']
+        Path(target_dir).mkdir(exist_ok=True)
+
+        if file:
+            path = os.path.join(source_dir, file)
+            if os.path.isfile(path):
+                bookId = getBookId(file)
+                if bookId:
+                    check_dups(bookId, file)
+                title = convertFile(path, bookId)
+            else:
+                reportError(f"No such file: {path}")
+        else:
+            convertFolder(source_dir)
+
+    reportStatus("\nDone.")
+    sys.stdout.flush()
+    if gui:
+        gui.event_generate('<<ScriptEnd>>', when="tail")
+
+# Processes each directory and its files one at a time
+if __name__ == "__main__":
+    main()
