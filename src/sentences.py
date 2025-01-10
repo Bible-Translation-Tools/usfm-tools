8872f47c6c9cb339f46479e1caeeee36cbed3fbf
Larry Versaw <lversaw@gmail.com>
2025-01-10 11:06:54 -0700

    Devangari, Ethiopic support, and a new function

--- src/sentences.py
+++ src/sentences.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 # Utility functions for detecting end of sentence, and for capitalizing the first word in sentences.
+# Note that these functions do not work if there are USFM markers in the input strings.
 
 import re
 
 firstword_re = re.compile(r'([\w]+)')
-nextsent_re = re.compile(r'[.?!\u0964].*?([\w]+)')
-specialquoted_re = re.compile(r'[?!\u0964\u1362][\'"’”»\-——]')    # exception to sentence ending
-endsentence_re = re.compile(r'[.?!\u0964\u1362][^\w]*$')
+nextsent_re = re.compile(r'[.?!\u0964\u1361\u1362].*?([\w]+)')
+specialquoted_re = re.compile(r'[?!\u0964\u1361\u1362][\'"’”»\-——]')    # exception to sentence ending
+endsentence_re = re.compile(r'[.?!\u0964\u1361\u1362][^\w]*$')
+
+"""
+Special characters:
+\u0964 is the Devangari Danda । character that terminates a sentence.
+\u1361 is the Ethiopic Wordspace character that is often used doubled up to use in place of \u1362.
+\u1362 is the Ethiopic Full Stop ። character that terminates a sentence.
+"""
 
 # Returns True if the specified text ends with sentence-ending punctuation:
 #    period, question mark, exclamation mark, or the Indian character । (\u0964)
@@ -37,6 +45,15 @@ def nextfirstwords(str):
         if not specialquoted_re.match(str[next.start():next.start()+2]):
             yield next.group(1)
         next = nextsent_re.search(str, next.end())
+
+# Generator function to yield the starting position of each sentence
+# or partial sentence in str.
+def nextstartpos(str):
+    next = firstword_re.search(str)
+    while next:
+        nextpos = next.start(1)
+        yield nextpos
+        next = nextsent_re.search(str, nextpos+1)
 
 # Capitalizes the first word in each sentence in the string.
 # Capitalizes the first word in the string if startsSentence is True.
