2e1d38cc3c293c088a6ffcd1731c169c5ab56917
Larry Versaw <lversaw@gmail.com>
2025-01-11 16:33:36 -0700

    new support for Ethiopic and section titles

--- src/sentences.py
+++ src/sentences.py
@@ -5,19 +5,18 @@
 import re
 
 firstword_re = re.compile(r'([\w]+)')
-nextsent_re = re.compile(r'[.?!\u0964\u1361\u1362].*?([\w]+)')
+nextsent_re = re.compile(r'[.?!\u0964\u1361\u1362].*?([\w]+)', re.DOTALL)
 specialquoted_re = re.compile(r'[?!\u0964\u1361\u1362][\'"’”»\-——]')    # exception to sentence ending
 endsentence_re = re.compile(r'[.?!\u0964\u1361\u1362][^\w]*$')
 
 """
 Special characters:
 \u0964 is the Devangari Danda । character that terminates a sentence.
-\u1361 is the Ethiopic Wordspace character that is often used doubled up to use in place of \u1362.
+\u1361 is the Ethiopic Wordspace ፡ character that is often doubled up to use in place of \u1362.
 \u1362 is the Ethiopic Full Stop ። character that terminates a sentence.
 """
 
 # Returns True if the specified text ends with sentence-ending punctuation:
-#    period, question mark, exclamation mark, or the Indian character । (\u0964)
 def endsSentence(str):
     return endsentence_re.search(str)
 
@@ -38,7 +37,7 @@ def firstword(str):
     return word
 
 # Generator function to yield the first word in each sentence in str,
-# not counting the first word in the string, even if it starts a sentence.
+# ***not counting*** the first word in the string, even if it starts a sentence.
 def nextfirstwords(str):
     next = nextsent_re.search(str)
     while next:
@@ -51,9 +50,10 @@ def nextfirstwords(str):
 def nextstartpos(str):
     next = firstword_re.search(str)
     while next:
-        nextpos = next.start(1)
-        yield nextpos
-        next = nextsent_re.search(str, nextpos+1)
+        if not specialquoted_re.match(str[next.start():next.start()+2]):
+            nextpos = next.start(1)
+            yield nextpos
+        next = nextsent_re.search(str, next.end())
 
 # Capitalizes the first word in each sentence in the string.
 # Capitalizes the first word in the string if startsSentence is True.
@@ -74,3 +74,8 @@ def capitalize(str, startsSentence):
                 changed = True
         next = nextsent_re.search(str, next.end())
     return str
+
+# Returns the number of sentences or partial sentences in the string.
+def sentenceCount(str):
+    startposlist = [pos for pos in nextstartpos(str)]
+    return len(startposlist)
