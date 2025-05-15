8a449b100ca3eea88019343c66eff34c9b7a0cfd
Larry Versaw <lversaw@gmail.com>
2025-05-15 11:45:41 -0600

    new unit tests

--- tests/test_verifyUsfm.py
+++ tests/test_verifyUsfm.py
@@ -146,3 +146,17 @@ def test_validBracketedFootnote(text, expected):
     ])
 def test_peripheral(fname, expected):
     assert verifyUSFM.peripheral(fname) == expected
+
+@pytest.mark.parametrize('line, expected',
+    [
+        (None, None),
+        ('', None),
+        ('A line, with nothing quoted.', None),
+        ('\\v 1 A real, "test"', 'real'),
+        ('\\v 2 A colon: «test', 'colon'),
+        ('\\v 3 A single quote, \'quotation.\'', None),
+        ('\\v 4 A backward, ”quotation.“', None),
+    ])
+def test_said_word(line, expected):
+    word = verifyUSFM.said_word(line)
+    assert word == expected
