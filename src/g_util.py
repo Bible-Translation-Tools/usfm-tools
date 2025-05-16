f08a377c118091b9a1490614c9b3608faf82e94b
Larry Versaw <lversaw@gmail.com>
2025-05-15 22:49:48 -0600

    non recursive file count

--- src/g_util.py
+++ src/g_util.py
@@ -4,18 +4,13 @@ import os
 import re
 
 # Returns a count of files in specifed folder matching file name pattern.
-# Recursive
-# Omits subfolders whose names start with '.'
+# Non-recursive
 def count_files(folder, pattern):
     n = 0
     if os.path.isdir(folder):
         for entry in os.listdir(folder):
-            if entry[0] != '.':
-                path = os.path.join(folder, entry)
-                if os.path.isdir(path):
-                    n += count_files(path, pattern)
-                elif re.match(pattern, entry.lower()):
-                    n += 1
+            if re.match(pattern, entry.lower()):
+                n += 1
     return n
 
 # Returns a count of folders in path matching path name pattern.
