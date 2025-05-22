90384742f07642985d761c668aa19f5a89507e85
Larry Versaw <lversaw@gmail.com>
2025-05-22 12:15:36 -0600

    v. 1.4

--- src/usfm_wizard.py
+++ src/usfm_wizard.py
@@ -39,7 +39,7 @@ from usx2usfm import main
 from verifyManifest import main
 from word2text import main
 
-app_version = "1.3.6"
+app_version = "1.4.0"
 
 class UsfmWizard(tkinter.Tk):
     def __init__(self):
