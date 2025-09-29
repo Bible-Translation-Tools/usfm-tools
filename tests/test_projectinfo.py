32cfed6ffe60918e33284fc8ebd4afa5ea609ddd
Larry Versaw <lversaw@gmail.com>
2025-09-29 08:58:21 -0600

    projectInfo API change

--- tests/test_projectinfo.py
+++ tests/test_projectinfo.py
@@ -1,6 +1,6 @@
 # pytest unit tests for functions in projectinfo.py
 # Before running all the tests as a whole:
-#   Manifest.yaml should have valid language info.
+#   Manifest.yaml in test_dir should have valid language info.
 #   Manifest.yaml should each have one valid source.
 #   remove "said_words" from test.json, or set it to an empty dict -- {}
 
@@ -13,7 +13,7 @@ sys.path.append(src_path)
 from projectinfo import ProjectInfo, SaidWords
 from manifestyaml import ManifestYaml
 
-dir = r'C:\DCS\Test\test_pi'
+test_dir = r'C:\DCS\Test\test_pi'
 language_code = 'test-pi'
 language_name = 'Test ProjectInfo'
 source_lang = 'test-source'
@@ -22,47 +22,47 @@ source_ver = "1.8"
 
 def test_init_newfile():
     # This test function backs up the existing .json file before deleting it.
-    path = os.path.join(os.path.dirname(dir), language_code+'.json')
+    path = os.path.join(os.path.dirname(test_dir), language_code+'.json')
     bakpath = path + ".bak"
     if os.path.exists(path):
         if not os.path.exists(bakpath):
             os.rename(path, bakpath)
         else:
             os.remove(path)
-    projectInfo = ProjectInfo(dir, language_code)
-    projectInfo.useManifest()
+    projectInfo = ProjectInfo(test_dir, language_code)
+    projectInfo.useManifest(docreate=True)
     assert projectInfo.getLanguageCode() == language_code
     projectInfo.save()
     assert projectInfo.getLanguageCode() == language_code
 
 def test_init_badid():
     newcode = 'xyz'
-    projectInfo = ProjectInfo(dir, newcode)
+    projectInfo = ProjectInfo(test_dir, newcode)
     assert projectInfo.getLanguageCode() == newcode
 
 def test_init_noid():
     newcode = ''
-    projectInfo = ProjectInfo(dir, newcode)
+    projectInfo = ProjectInfo(test_dir, newcode)
     assert projectInfo.getLanguageCode() == newcode
     projectInfo.save()
-    projectInfo = ProjectInfo(dir, newcode)
+    projectInfo = ProjectInfo(test_dir, newcode)
     assert projectInfo.getLanguageCode() == newcode
 
 def test_init_oldfile():
-    projectInfo = ProjectInfo(dir, language_code)
+    projectInfo = ProjectInfo(test_dir, language_code)
     assert projectInfo.getLanguageCode() == language_code
-    projectInfo = ProjectInfo(dir, '')
+    projectInfo = ProjectInfo(test_dir, '')
     assert projectInfo.getLanguageCode() == ''
 
 def test_language_name():
-    projectInfo = ProjectInfo(dir, language_code)
+    projectInfo = ProjectInfo(test_dir, language_code)
     projectInfo.setLanguage(language_name, 'rtl')
     projectInfo.save()
     assert projectInfo.getLanguageName() == language_name
 
 def test_mainsource():
     my = ManifestYaml()
-    my.load(dir)
+    my.load(test_dir)
     sources = my.getSources()
     if len(sources) > 0:
         assert sources[0]['language'] == source_lang
@@ -71,10 +71,10 @@ def test_mainsource():
     else:
         my.addSource(source_lang, source_resource_id, source_ver)
     my.save()
-    pi = ProjectInfo(dir, language_code)
+    pi = ProjectInfo(test_dir, language_code)
     pi.resetSources()
     assert pi.getMainSource() == None
-    pi.useManifest()
+    pi.useManifest(docreate=True)
     source = pi.getMainSource()
     assert source['language_id'] == source_lang
     assert source['resource_id'] == source_resource_id
@@ -83,13 +83,13 @@ def test_mainsource():
 
 def test_sources():
     add1source('swedish', 'bible', '1.99')
-    projectInfo = ProjectInfo(dir, language_code)
+    projectInfo = ProjectInfo(test_dir, language_code)
     projectInfo.addSource('en', 'ulb', '7.6')
     projectInfo.addSource('en', 'ulb', '7.6')
     projectInfo.addSource('en', 'ulb', '2')
     assert len(projectInfo.getSources()) == 3
     projectInfo.save()
-    pi2 = ProjectInfo(dir, language_code)
+    pi2 = ProjectInfo(test_dir, language_code)
     source = pi2.getMainSource()
     assert source['version'] == "7.6"
 
@@ -97,7 +97,7 @@ def add1source(lang, resource_id, ver):
     lang = 'swedish'
     # resource_id = 'bible'
     ver = '1.99'
-    projectInfo = ProjectInfo(dir, language_code)
+    projectInfo = ProjectInfo(test_dir, language_code)
     projectInfo.addSource(lang, resource_id, ver)
     assert projectInfo.knownSource(lang, resource_id, ver) == True
     projectInfo.resetSources()
@@ -130,9 +130,9 @@ def test_manifest_connection():
     mylen = len(my.getSources())
     assert mylen == 1   # Necessary for the following tests to work
 
-    pi.useManifest()        # sync happens here
+    pi.useManifest(docreate=True)        # sync happens here
     pilen = len(pi.getSources())
-    assert pilen == 1  # the one is from the sync from manifest
+    assert pilen == 1  # it got one from the sync from manifest
     piname = pi.getLanguageName()
     assert piname != ""
     pi.save()
@@ -169,8 +169,8 @@ def test_manifest_connection():
 # Before running this, remove "said_words" from test.json.
 # Or, set it to an empty dict -- {}
 def test_saidwords():
-    global dir
-    dir = r'C:\DCS\Test\test_reg'
+    global test_dir
+    test_dir = r'C:\DCS\Test\test_reg'
     global language_code
     language_code = 'test'
 
@@ -182,12 +182,12 @@ def test_saidwords():
     savedSaidWords( )
 
 def nowords():
-    pi = ProjectInfo(dir, language_code)
+    pi = ProjectInfo(test_dir, language_code)
     words = pi.getWords()
     assert words == []
 
 def addwords():
-    pi = ProjectInfo(dir, language_code)
+    pi = ProjectInfo(test_dir, language_code)
     pi.addWord('aaa', 2)
     pi.addWord('bbb', 3)
     pi.addWord('ccc', 1)
@@ -200,14 +200,14 @@ def addwords():
 
 # Run this test after running test_addwords()
 def savedwords():
-    pi = ProjectInfo(dir, language_code)
+    pi = ProjectInfo(test_dir, language_code)
     words = pi.getWords(mincount=1)
     assert words == ['aaa', 'bbb', 'ccc']
     words = pi.getWords(mincount=2)
     assert words == ['aaa', 'bbb']
 
 def add_to_savedwords():
-    pi = ProjectInfo(dir, language_code)
+    pi = ProjectInfo(test_dir, language_code)
     pi.addWord('bbb', 1)    # no effect
     pi.addWord('ccc', 3)
     words = pi.getWords(mincount=2)
@@ -216,7 +216,7 @@ def add_to_savedwords():
 
 # Run this test after running test_savedwords()
 def saidWords():
-    saidwords = SaidWords(dir, language_code)
+    saidwords = SaidWords(test_dir, language_code)
     saidwords.addWord('aaa')
     saidwords.addWord('aaa')
     saidwords.addWord('aaa')
@@ -247,7 +247,7 @@ def test_addSourceDir():
     assert pi.getSourceDir() == sourcedir
 
 def savedSaidWords():
-    pi = ProjectInfo(dir, language_code)
+    pi = ProjectInfo(test_dir, language_code)
     words = pi.getWords(mincount=1)
     assert words == ['aaa', 'bbb', 'ccc', 'ddd']    # notice, 'eee' was not saved
     words = pi.getWords(mincount=3)
